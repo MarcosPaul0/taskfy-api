c0bb8df2231b32dfd74900b516dbaba555b5fa0c
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-02-03 07:36:40 -0300

    feat: adiciona constantes para aplicação

---
+++ src/main/java/com/api/taskfy/constants/Routes.java
@@ -0,0 +1,11 @@
+package com.api.taskfy.constants;
+
+public class Routes {
+    public static final String AUTH = "/auth";
+    public static final String USER = "/user";
+    public static final String TASK_GROUP = "/task-group";
+    public static final String TASK_GROUP_USER = "/task-group-user";
+    public static final String TASK = "/task";
+    public static final String RANKING = "/ranking";
+    public static final String REWARD = "/reward";
+}
---
+++ src/main/java/com/api/taskfy/constants/Tables.java
@@ -0,0 +1,12 @@
+package com.api.taskfy.constants;
+
+public class Tables {
+    public static final String USER = "taskfy_user";
+    public static final String TASK_GROUP = "taskfy_task_group";
+    public static final String REFRESH_TOKEN = "taskfy_refresh_token";
+    public static final String TASK_GROUP_USER = "taskfy_task_group_user";
+    public static final String TASK = "taskfy_task";
+    public static final String RANKING = "taskfy_ranking";
+    public static final String REWARD = "taskfy_reward";
+    public static final String USER_PLACEMENT = "taskfy_user_placement";
+}
