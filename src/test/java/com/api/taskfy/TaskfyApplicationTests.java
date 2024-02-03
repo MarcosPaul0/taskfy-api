ea03c5c3abc40cca1e37d341d19b2acaa25408cc
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-02-03 07:21:07 -0300

    feature: arquivos de configuração iinicial

---
+++ src/test/java/com/api/taskfy/TaskfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.taskfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
