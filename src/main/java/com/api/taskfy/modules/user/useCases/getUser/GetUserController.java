1349d4b231f0f9a2ee842d74952fbcb81a85ad1a
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-02-04 13:49:38 -0300

    feat: implementa rota de busca de usuário pelo token

---
+++ src/main/java/com/api/taskfy/modules/user/useCases/getUser/GetUserController.java
@@ -0,0 +1,20 @@
+package com.api.taskfy.modules.user.useCases.getUser;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.api.taskfy.constants.Routes;
+
+@RestController
+@RequestMapping(Routes.USER)
+public class GetUserController {
+
+  @GetMapping
+  public ResponseEntity<UserDetails> handle(@AuthenticationPrincipal UserDetails user) {
+    throw new RuntimeException();
+
+  }
+}
