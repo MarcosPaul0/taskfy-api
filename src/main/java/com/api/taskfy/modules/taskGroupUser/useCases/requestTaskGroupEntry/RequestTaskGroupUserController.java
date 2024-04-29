3d8dc0e8d5f7505b134d2963b9a91f790eaaca4d
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-04-29 14:28:29 -0300

    feat: implementa solicitação de grupo de tarefas

---
+++ src/main/java/com/api/taskfy/modules/taskGroupUser/useCases/requestTaskGroupEntry/RequestTaskGroupUserController.java
@@ -0,0 +1,24 @@
+package com.api.taskfy.modules.taskGroupUser.useCases.requestTaskGroupEntry;
+
+import com.api.taskfy.constants.Routes;
+import com.api.taskfy.modules.taskGroupUser.dtos.CreateTaskGroupUserDto;
+import com.api.taskfy.modules.user.entities.User;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(Routes.TASK_GROUP_USER)
+public class RequestTaskGroupUserController {
+    @Autowired
+    RequestTaskGroupUserService requestTaskGroupUserService;
+
+    @PostMapping("/send/request/{groupId}")
+    public ResponseEntity<Void> handle(@PathVariable("groupId") String groupId, @AuthenticationPrincipal User user) {
+        this.requestTaskGroupUserService.execute(groupId, user.getId());
+
+        return ResponseEntity.ok().build();
+    }
+}
