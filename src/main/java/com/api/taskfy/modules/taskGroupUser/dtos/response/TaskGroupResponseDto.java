35154b0cb6f8dde0aa325d69e0cc58040ee899dc
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-05-02 16:03:46 -0300

    feat: adiciona dto de resposta de convite customizado

---
+++ src/main/java/com/api/taskfy/modules/taskGroupUser/dtos/response/TaskGroupResponseDto.java
@@ -0,0 +1,12 @@
+package com.api.taskfy.modules.taskGroupUser.dtos.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskGroupResponseDto {
+    private String Id;
+    private String name;
+    private String description;
+}
