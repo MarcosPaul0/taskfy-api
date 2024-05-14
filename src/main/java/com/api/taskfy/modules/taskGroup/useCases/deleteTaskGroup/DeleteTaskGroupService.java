db4da56428daf0e657e74adcc90fcc3fcf493f4a
MarcosPaul0 <marcos.pereira@hvex.com.br>
2024-05-14 15:39:46 -0300

    feat: implementa serviço de deleção de tarefas

---
+++ src/main/java/com/api/taskfy/modules/taskGroup/useCases/deleteTaskGroup/DeleteTaskGroupService.java
@@ -0,0 +1,17 @@
+package com.api.taskfy.modules.taskGroup.useCases.deleteTaskGroup;
+
+import com.api.taskfy.errors.taskGroup.TaskGroupNotFoundException;
+import com.api.taskfy.errors.user.UserNotFoundException;
+import com.api.taskfy.modules.taskGroup.repositories.TaskGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteTaskGroupService {
+    @Autowired
+    TaskGroupRepository taskGroupRepository;
+
+    public void execute(String groupId) {
+        this.taskGroupRepository.deleteById(groupId);
+    }
+}
